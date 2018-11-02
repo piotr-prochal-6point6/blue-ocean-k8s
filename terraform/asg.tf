25f020551b4cd2bf2bda545af7f790477f2346ca
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-02 08:45:41 +0100

    Worker node ASG config

---
+++ terraform/asg.tf
@@ -0,0 +1,21 @@
+#Autoscaling Groups
+resource "aws_autoscaling_group" "blue_ocean_eks" {
+  desired_capacity     = 2
+  launch_configuration = "${aws_launch_configuration.blue_ocean_eks.id}"
+  max_size             = 2
+  min_size             = 1
+  name                 = "blue-ocean-eks"
+  vpc_zone_identifier  = ["${data.terraform_remote_state.infrastructure.private_subnet_ids}"]
+
+  tag {
+    key                 = "Name"
+    value               = "blue-ocean-eks"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "kubernetes.io/cluster/${var.cluster_name}"
+    value               = "owned"
+    propagate_at_launch = true
+  }
+}
---
+++ terraform/launch_configuration.tf
@@ -0,0 +1,14 @@
+#Launch Confiuration
+resource "aws_launch_configuration" "blue_ocean_eks" {
+  associate_public_ip_address = true
+  iam_instance_profile        = "${aws_iam_instance_profile.blue_ocean_eks_node.name}"
+  image_id                    = "${data.aws_ami.eks_worker.id}"
+  instance_type               = "m4.large"
+  name_prefix                 = "blue-ocean-eks-node"
+  security_groups             = ["${aws_security_group.blue_ocean_node.id}"]
+  user_data_base64            = "${base64encode(${file(../userdata/worker_node_userdata.sh)})}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
