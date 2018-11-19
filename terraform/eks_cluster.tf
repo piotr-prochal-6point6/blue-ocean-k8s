83e7b97417a2cd93478f481a1dcea08d1584d900
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-19 07:40:03 -0500

    Missed the security group name in the refactoring

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -4,7 +4,7 @@ resource "aws_eks_cluster" "blue_ocean" {
   role_arn = "${aws_iam_role.blue_ocean_eks_cluster.arn}"
 
   vpc_config {
-    security_group_ids = ["${aws_security_group.blue_ocean_lead_cluster.id}"]
+    security_group_ids = ["${aws_security_group.blue_ocean_cluster_lead.id}"]
     subnet_ids         = ["${data.terraform_remote_state.infrastructure.private_subnet_ids}"]
   }
 
