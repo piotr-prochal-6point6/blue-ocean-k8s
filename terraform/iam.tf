32ee6923f996897b00197f6c1c9aed5bc44e83a6
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-18 20:45:17 -0500

    Minor typing fix

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,12 +1,12 @@
 #Identity and Access Management
 resource "aws_iam_role" "blue_ocean_eks_cluster" {
   name               = "blue-ocean-eks-cluster"
-  assume_role_policy = "${file(../iam/blue-ocean-eks-cluster-policy.json)}"
+  assume_role_policy = "${file("../iam/blue-ocean-eks-cluster-policy.json")}"
 }
 
 resource "aws_iam_role" "blue_ocean_eks_node" {
   name               = "blue-ocean-eks-node"
-  assume_role_policy = "${file(../iam/blue-ocean-eks-node-policy.json)}"
+  assume_role_policy = "${file("../iam/blue-ocean-eks-node-policy.json")}"
 }
 
 resource "aws_iam_role_policy_attachment" "blue_ocean_eks_cluster_AmazonEKSClusterPolicy" {
