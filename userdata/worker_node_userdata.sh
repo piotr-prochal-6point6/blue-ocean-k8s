af73dbc1c88651c1169b0f24e7ad4efa79071b4c
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-19 09:29:16 -0500

    Trying to get the worker nodes to register to the cluster

--- userdata/worker_node_userdata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.blue_ocean_lead.endpoint}' --b64-cluster-ca '${aws_eks_cluster.blue_ocean_lead.certificate_authority.0.data}' '${var.cluster_name}'
