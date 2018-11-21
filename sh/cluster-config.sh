4b1a2d9f5c41bf1f146df00b94c9c145e0d9846a
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-21 13:05:32 -0500

    Made the gp2 storageclass default

--- sh/cluster-config.sh
+++ sh/cluster-config.sh
@@ -24,6 +24,9 @@ do
   fi
 done
 
+kubectl patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl get storageclass
+
 # Initialise Helm
 helm init --service-account tiller
 
