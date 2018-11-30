f218161a9abcf494b06c77494c15328cc9083b04
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-30 19:35:49 +0100

    Using a simpler nginx config

--- terraform/nginxconf.tf
+++ terraform/nginxconf.tf
@@ -1,64 +1,35 @@
 locals {
-  nginxconf = <<EOF
-upstream jenkins {
-  keepalive 32; # keepalive connections
-  server 127.0.0.1:8080; # jenkins ip and port
+nginxconf = <<EOF
+server {
+    listen 80;
+    server_name ${aws_route53_record.www.name};
+    return 301 https://$host$request_uri;
 }
 
 server {
-  listen          80;       # Listen on port 80 for IPv4 requests
 
-  server_name     ${aws_route53_record.www.name};
+    listen 80;
+    server_name ${aws_route53_record.www.name};
 
-  #this is the jenkins web root directory (mentioned in the /etc/default/jenkins file)
-  root            /var/run/jenkins/war/;
+    location / {
 
-  access_log      /var/log/nginx/jenkins/access.log;
-  error_log       /var/log/nginx/jenkins/error.log;
-  ignore_invalid_headers off; #pass through headers from Jenkins which are considered invalid by Nginx server.
+      proxy_set_header        Host $host:$server_port;
+      proxy_set_header        X-Real-IP $remote_addr;
+      proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header        X-Forwarded-Proto $scheme;
 
-  location ~ "^/static/[0-9a-fA-F]{8}\/(.*)$" {
-    #rewrite all static files into requests to the root
-    #E.g /static/12345678/css/something.css will become /css/something.css
-    rewrite "^/static/[0-9a-fA-F]{8}\/(.*)" /$1 last;
-  }
+      # Fix the "It appears that your reverse proxy set up is broken" error.
+      proxy_pass          http://127.0.0.1:8080;
+      proxy_read_timeout  90;
 
-  location /userContent {
-    #have nginx handle all the static requests to the userContent folder files
-    #note : This is the $JENKINS_HOME dir
-  root /var/lib/jenkins/;
-    if (!-f $request_filename){
-      #this file does not exist, might be a directory or a /**view** url
-      rewrite (.*) /$1 last;
-    break;
-    }
-  sendfile on;
-  }
+      proxy_redirect      http://127.0.0.1:8080 https://${aws_route53_record.www.name};
 
-  location / {
-      sendfile off;
-      proxy_pass         http://jenkins;
-      proxy_redirect     default;
+      # Required for new HTTP-based CLI
       proxy_http_version 1.1;
-
-      proxy_set_header   Host              $host;
-      proxy_set_header   X-Real-IP         $remote_addr;
-      proxy_set_header   X-Forwarded-For   $proxy_add_x_forwarded_for;
-      proxy_set_header   X-Forwarded-Proto $scheme;
-      proxy_max_temp_file_size 0;
-
-      #this is the maximum upload size
-      client_max_body_size       10m;
-      client_body_buffer_size    128k;
-
-      proxy_connect_timeout      90;
-      proxy_send_timeout         90;
-      proxy_read_timeout         90;
-      proxy_buffering            off;
-      proxy_request_buffering    off; # Required for HTTP CLI commands in Jenkins > 2.54
-      proxy_set_header Connection ""; # Clear for keepalive
-  }
-
+      proxy_request_buffering off;
+      # workaround for https://issues.jenkins-ci.org/browse/JENKINS-45651
+      add_header 'X-SSH-Endpoint' '${aws_route53_record.www.name}:50022' always;
+    }
 }
 EOF
 }
