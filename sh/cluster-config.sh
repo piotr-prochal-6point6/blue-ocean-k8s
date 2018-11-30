e1cc67be862254e6fa6f2279bd81d227072be8ab
Hartnell Foster <hartnell.foster@googlemail.com>
2018-11-30 21:07:57 +0100

    No more Helm or Tiller

--- sh/cluster-config.sh
+++ sh/cluster-config.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-# Join the worker nodes to the cluster, configure service accounts
+# Configure and join worker nodes to the cluster
 
 PARENT_DIR=${PWD%/*}
 KUBE_HOME=${HOME}/.kube
 KUBECONFIG="config.eks"
-OUTPUTS="config-map-aws-auth storageclass rbac-config"
+OUTPUTS="config-map-aws-auth storageclass"
 
 cd ${PARENT_DIR}/terraform
 terraform output ${KUBECONFIG} > ${KUBE_HOME}/${KUBECONFIG}
 rm ${KUBE_HOME}/config
 ln -s ${KUBE_HOME}/${KUBECONFIG} ${KUBE_HOME}/config
-
-# Create tiller service account
-kubectl create serviceaccount tiller --namespace kube-system
 
 for conf in ${OUTPUTS}
 do
@@ -27,8 +24,5 @@ done
 
 kubectl patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl get storageclass
-
-# Initialise Helm
-#helm init --service-account tiller
 
 echo "Basic cluster configuration complete!"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,10 +10,6 @@ output "storageclass" {
   value = "${local.storageclass}"
 }
 
-output "rbac-config" {
-  value = "${local.rbac_config}"
-}
-
 output "ecr_repo_url" {
   value = "${aws_ecr_repository.internal.repository_url}"
 }
