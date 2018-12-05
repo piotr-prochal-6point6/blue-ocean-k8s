29a76847e23030d17ffd80141d5c09bca5931ab8
Hartnell Foster <hartnell.foster@googlemail.com>
2018-12-05 22:23:17 +0100

    Missed one...

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,6 @@ output "config-map-aws-auth" {
   value = "${local.config_map_aws_auth}"
 }
 
-output "storageclass" {
-  value = "${local.storageclass}"
-}
-
 output "ecr_repo_url" {
   value = "${aws_ecr_repository.internal.repository_url}"
 }
